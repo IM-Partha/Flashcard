54b6fc851dc36355a583bfa9b6013d56075c32dc
Partha Debnath <85936227+IM-Partha@users.noreply.github.com>
2025-09-25 11:11:25 +0530

    new feature added

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,12 +1,29 @@
 import * as yup from 'yup'
+
 // validation for input field 
 export const signUpSchema = yup.object({
-    Create_Group: yup.string().min(5).max(20).required("Please Enter Group Name") ,
-    description: yup.string().min(20).max(300).required("Please Enter Description") ,
+    Create_Group: yup
+        .string()
+        .min(5, "Group Name must be at least 5 characters")
+        .max(50, "Group Name must be at most 50 characters")
+        .required("Please Enter Group Name"),
+        
+    description: yup
+        .string()
+        .min(20, "Description must be at least 20 characters")
+        .required("Please Enter Description"),
+        
     term: yup.array(
         yup.object({
-            Enter_Term: yup.string().min(5, 'Term must be at least 5 characters').max(20,'Term must be at most 20 characters').required("Please Enter Term") ,
-            Enter_Definition: yup.string().min(10, 'Definition must be at least 10 characters').max(200,'Definition must be at most 200 characters').required("Please Enter Definition") ,
+            Enter_Term: yup
+                .string()
+                .min(2, "Term must be at least 5 characters")
+                .required("Please Enter Term"),
+                
+            Enter_Definition: yup
+                .string()
+                .min(10, "Definition must be at least 10 characters")
+                .required("Please Enter Definition"),
         })
     ),
 });
